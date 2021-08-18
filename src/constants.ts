ae8db580e29b9acefda811c80952652adb0e3e35
phungdaihiep <phungdaihiep.extracy@gmail.com>
2021-08-18 23:09:15 +0700

    change color

--- src/constants.ts
+++ src/constants.ts
@@ -1,14 +1,14 @@
 export const colors = [
   "#01888C", // teal
-  "#FC7500", // bright orange
+  "#05222C", // bright orange
   "#034F5D", // dark teal
-  "#F73F01", // orangered
+  "#15313D", // orangered
   "#FC1960", // magenta
-  "#C7144C", // raspberry
-  "#F3C100", // goldenrod
-  "#1598F2", // lightning blue
+  "#35C4BA", // raspberry
+  "#FFFFFF", // goldenrod
+  "#8FA2B7", // lightning blue
   "#2465E1", // sail blue
-  "#F19E02", // gold
+  "#5F97FF", // gold
 ];
 
 export const wobble = 30;
